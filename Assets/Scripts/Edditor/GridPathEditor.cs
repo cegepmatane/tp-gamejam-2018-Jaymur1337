f467d29f78b5ea3d778ef883628ccd8c5ac7dde8
Jacob Murray <jaymur2010@gmail.com>
2018-11-23 20:31:16 -0500

    Ajout dossier edditor+Scripts

---
+++ Assets/Scripts/Edditor/GridPathEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridPath))]
+
+
+public class GridPathEditor : Editor
+{
+    private GridPath grid;
+
+    private void OnEnable()
+    {
+        grid = (GridPath)target;
+    }
+}
