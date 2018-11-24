c96baa97f008433d9adaf6de76c07aa4cecefe32
Jacob Murray <jaymur2010@gmail.com>
2018-11-24 10:53:33 -0500

    Level Gen

---
+++ Assets/Scripts/LevelGenerator/levelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour
+{
+    public BoardManager BoardScript;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        BoardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+    void InitGame()
+    {
+        BoardScript.SetupScene(3);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
