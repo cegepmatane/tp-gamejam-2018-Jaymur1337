1d3b68ccee52d789b0a29b6b8f92eedd0b654036
Unknown <samwil09@hotmail.ca>
2018-11-23 21:04:46 -0500

    Menu + scenes

--- Assets/Scripts/SamTest/MainMenuCTRL.cs
+++ Assets/Scripts/SamTest/MainMenuCTRL.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuCTRL : MonoBehaviour {
+public class MainMenuCTRL : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("InGame", LoadSceneMode.Single);
+    }
+
+    public void EditMapButton()
+    {
+        SceneManager.LoadScene("Editor", LoadSceneMode.Single);
+    }
+
+    public void ChoseMapButton()
+    {
+        
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
 }
