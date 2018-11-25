c8135a547002859f6b90826358c4435ee8369202
Jacob Murray <jaymur2010@gmail.com>
2018-11-25 15:02:10 -0500

    .

---
+++ Assets/Scripts/GameOver/MenuCTRL.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCTRL : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("TestJak", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+     
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/Scripts/GameOver/ScoreChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreChanger : MonoBehaviour
+{
+    public ScoreManager score;
+    public Text Text;
+
+    private void Awake()
+    {
+        score = GameObject.FindObjectOfType<ScoreManager>();
+        Text.text = "SCORE : " + score.Score;
+        Destroy(score.gameObject);
+    }
+}
