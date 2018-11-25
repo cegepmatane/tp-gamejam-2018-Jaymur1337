3dc34bdcf29f448ccc98e8439a2ddd8adce8fede
Jacob Murray <jaymur2010@gmail.com>
2018-11-24 22:49:47 -0500

    Fin Jour 2

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject Zombie;
+    public float MinSpawnTime = 1f;
+    public float MaxSpawnTime = 5f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        float t_InitialSpawnTime = Random.Range(MinSpawnTime, MaxSpawnTime);
+        StartCoroutine(SpawnZombie(t_InitialSpawnTime));
+    }
+	
+	
+
+    private IEnumerator SpawnZombie(float a_Delay)
+    {
+        yield return new WaitForSeconds(a_Delay); 
+
+        Instantiate(Zombie, this.transform.position, Quaternion.identity);
+
+
+        float t_NewDelay = Random.Range(MinSpawnTime, MaxSpawnTime);
+        StartCoroutine(SpawnZombie(t_NewDelay));
+
+
+    }
+}
